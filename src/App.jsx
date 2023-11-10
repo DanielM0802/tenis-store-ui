8a72475661c69fce273fb550f6799482c2d4ca0a
DanielM0802 <marillancadaniel@gmail.com>
2023-11-10 11:39:14 -0300

    react-router para pagina de pelotas y raquetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { NavbarTenis } from "./components/navbar/NavbarTenis"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home "
+import Pelotas from "./pages/productos/Pelotas"
+import Raquetas from "./pages/productos/Raquetas"
 
 function App() {
 
   return (
-    <>
+    <Router>
       <NavbarTenis/>
-      <h1>Tenis Store</h1>
-    </>
+      
+      <Routes>
+        <Route path="/" element={ <Home/> }/>
+        <Route path="/productos/pelotas" element={ <Pelotas/> }/>
+        <Route path="/productos/raquetas" element={ <Raquetas/> }/>
+
+      </Routes>
+
+    </Router>
   )
 }
 
---
+++ src/pages/productos/Pelotas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Producto } from '../../components/Producto'
+
+function Pelotas() {
+  return (
+    <div>
+      <h1>Pelotas</h1>
+      <Producto/>
+    </div>
+  )
+}
+
+export default Pelotas
