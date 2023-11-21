16e1cab682978d0ee5a140e3623de5f5c10519e2
DanielM0802 <marillancadaniel@gmail.com>
2023-11-21 12:41:23 -0300

    manejo de formulario y errores en formulario de registro

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -3,10 +3,50 @@ import {
     Input,
     Button,
     Typography,
+    Alert
   } from "@material-tailwind/react";
   import { NavLink } from "react-router-dom";
-   
+  import { useState } from "react"; 
+
   export function Registro() {
+
+    const [usuario, setUsuario] = useState({
+        nombre: "",
+        correo: "",
+        password: ""
+    })
+
+    const [error, setError] = useState();
+
+    const actualizarUsuario = (e) => {
+        const propiedad = e.target.name;
+        const valor = e.target.value;
+        //{ ...obj, key: 'value' }
+        setUsuario({ ...usuario,  [propiedad] : valor  })
+    }
+
+    const registrarUsuario = (e) => {
+        e.preventDefault();
+
+        console.log('tenemos que validar el objeto user: ')
+        console.log(usuario);
+
+        const {nombre, correo, password} = usuario;
+
+        if ( !nombre || !correo || !password ) {
+            setError("Todos los campos son obligatorios")
+            return;
+        }
+
+
+        console.log('registrando usuario...')
+        //llamar a ruta encargada de registrar usuarios
+
+    }
+
+    
+
+
     return (
        <Card color="transparent" shadow={false}>
         <Typography variant="h4" color="blue-gray">
@@ -15,12 +55,14 @@ import {
         <Typography color="gray" className="mt-1 font-normal">
           Ingresa tus datos para registrarte
         </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={registrarUsuario} >
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Tu nombre
             </Typography>
             <Input
+              name="nombre"
+              onChange={actualizarUsuario}    
               size="lg"
               placeholder="tu nombre"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
@@ -32,6 +74,8 @@ import {
               Tu correo
             </Typography>
             <Input
+              name="correo"
+              onChange={actualizarUsuario}    
               size="lg"
               placeholder="name@mail.com"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
@@ -43,6 +87,8 @@ import {
               Contraseña
             </Typography>
             <Input
+              name="password"
+              onChange={actualizarUsuario}    
               type="password"
               size="lg"
               placeholder="********"
@@ -53,7 +99,15 @@ import {
             />
           </div>
 
-          <Button className="mt-6" fullWidth>
+          {
+            error && 
+            <Alert color="red" className="my-4" variant="gradient">
+                <span>{error}</span>
+            </Alert>
+
+          }    
+
+          <Button type="submit" className="mt-6" fullWidth>
             Registrarse
           </Button>
           <Typography color="gray" className="mt-4 text-center font-normal">
